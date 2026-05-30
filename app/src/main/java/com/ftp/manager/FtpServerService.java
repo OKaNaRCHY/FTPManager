e8eafa763966a3e53d11a37a33f24913f69493f0
Fahri Okan OKYAY <okan.okyay@msn.com>
2026-05-30 12:13:41 +0300

    Update FtpServerService.java

--- app/src/main/java/com/ftp/manager/FtpServerService.java
+++ app/src/main/java/com/ftp/manager/FtpServerService.java
@@ -16,6 +16,7 @@ import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
 import org.apache.ftpserver.ftplet.Authority;
 import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.UserManager;
 import org.apache.ftpserver.listener.ListenerFactory;
 import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
 import org.apache.ftpserver.usermanager.impl.BaseUser;
@@ -48,7 +49,7 @@ public class FtpServerService extends Service {
             factory.addListener("default", lf.createListener());
 
             PropertiesUserManagerFactory umf = new PropertiesUserManagerFactory();
-            org.apache.ftpserver.usermanager.UserManager um = umf.createUserManager();
+            UserManager um = umf.createUserManager();
 
             BaseUser user = new BaseUser();
             user.setName(username);
